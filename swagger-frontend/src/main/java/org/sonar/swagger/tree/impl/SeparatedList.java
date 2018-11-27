7bddf9c2cb420f4b8aa8b9253739ccf43174a0cb
Zouhir OUFTOU <zouhir_ouftou@carrefour.com>
2018-11-27 23:25:17 +0100

    Add License and copyright files

--- swagger-frontend/src/main/java/org/sonar/swagger/tree/impl/SeparatedList.java
+++ swagger-frontend/src/main/java/org/sonar/swagger/tree/impl/SeparatedList.java
@@ -1,14 +1,34 @@
+/*
+ * SonarQube Swagger Analyzer
+ * Copyright (C) 2015-2017 Zouhir OUFTOU
+ * zouhir.ouftou@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.swagger.tree.impl;
-
-import com.google.common.base.Function;
-import com.google.common.base.Preconditions;
-import com.google.common.collect.UnmodifiableIterator;
-import org.sonar.plugins.swagger.api.tree.Tree;
 
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+
+import org.sonar.plugins.swagger.api.tree.Tree;
+
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.UnmodifiableIterator;
 
 public class SeparatedList<T> implements List<T> {
 
--- swagger-frontend/src/main/java/org/sonar/swagger/visitors/SyntaxHighlighterVisitor.java
+++ swagger-frontend/src/main/java/org/sonar/swagger/visitors/SyntaxHighlighterVisitor.java
@@ -1,3 +1,22 @@
+/*
+ * SonarQube Swagger Analyzer
+ * Copyright (C) 2015-2017 Zouhir OUFTOU
+ * zouhir.ouftou@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.swagger.visitors;
 
 import com.google.common.collect.ImmutableList;
